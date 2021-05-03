43124f50685a40343bb5d6f0236e479002c0eaac
marcoweb <marcocarvalho.web@gmail.com>
2021-05-03 18:54:15 +0000

    Livro

---
+++ src/main/java/application/models/Livro.java
@@ -0,0 +1,43 @@
+package application.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+
+@Entity
+@Table(name="livros")
+public class Livro {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    private String titulo;
+
+    @ManyToOne
+    @JoinColumn(name="id_genero")
+    private Genero genero;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return this.id;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getTitulo(){
+        return this.titulo;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+    public Genero getGenero() {
+        return this.genero;
+    }
+}
